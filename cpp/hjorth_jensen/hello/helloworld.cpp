abdd1bc8fe4af9ec948ba5a06320ac326609e071
Daniel Vander-Hyde <dvande72hyde@hotmail.com>
2024-05-16 20:39:14 -0400

    First iteration of Computational physics exercises from mhjensesn at MSU

---
+++ cpp/hjorth_jensen/hello/helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include <cmath>  
+using namespace std;
+int main (int argc, char* argv[])
+{
+    double r, s; 	// declare variables
+    r = atof(argv[1]); 	// convert the text argv[1] to double
+    s = sin(r);
+    cout << "Hello, World! sin(" << r << ") =" << s << endl;
+    return 0; 		//success execution of the program
+}
